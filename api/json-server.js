900229381ae9ad8497bcb4111b93ffec1de5d721
Saeid Mohadjer <saeid@fastmail.fm>
2024-02-09 10:05:36 +0100

    moves json to app folder

--- api/json-server.js
+++ api/json-server.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async (req, res) => {
-	const jsonPath = path.join(process.cwd(), 'db.json');
+	const jsonPath = path.join(process.cwd(), 'src', 'app', 'db.json');
   console.log(jsonPath);
 	const jsonFile = fs.readFileSync(jsonPath, 'utf8');
 	const json = JSON.parse(jsonFile);
-	return res.status(200).json(json);
+  res.setHeader('Content-Type', 'application/json');
+	return res.json(json);
 }
